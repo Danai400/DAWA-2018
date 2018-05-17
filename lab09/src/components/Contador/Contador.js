7747bfaa1565902072270d66d6db751cc25c9665
Yohana <yohana.perez@tecsup.edu.pe>
2018-05-17 00:58:17 -0500

    lab 10 terminado

--- lab09/src/components/Contador/Contador.js
+++ lab09/src/components/Contador/Contador.js
@@ -1,14 +1,42 @@
 import React, {Component} from 'react';
+import classes from './Contador.css';
 
 export default class Contador extends Component {
+	state = {
+		cont: this.props.valor
+}
+		disminuirHandler = () => {
+			this.setState({
+				cont: this.state.cont -1
+			});
+		}
+		aumentarHandler = () => {
+			this.setState({
+				cont: this.state.cont +1
+			});
+		}
+		resetHandler = () => {
+			this.setState({
+				cont: this.props.valor
+			});
+		}
+	
 	render(){
-		let advertencia = (<p>Iniciamos con un número mayor a 5!</p>);
-		if(this.props.valor<=5) advertencia = null;
+		let advertencia = (<p> Iniciamos con un nro mayor a 5!</p>)
 
-		return (<div>
-		<h1>este es mi componente de contador</h1>
-		<p>Este contador iniciará en: {this.props.valor}</p>
-		{advertencia}
-		</div>)
+	if(this.props.valor<=5) advertencia = null;
+			
+		return (<div className={classes.Contador}>
+			<h1>Esta es mi calculadora con react</h1>
+		
+			<p> La suma de los numeros es : {this.props.valor}</p>
+			{advertencia}
+			<p>Numero Actual: {this.state.cont}</p>
+			<button onClick={this.disminuirHandler}>Disminuir</button>
+			<button onClick={this.resetHandler}>Resetear</button>
+			<button onClick={this.aumentarHandler}>Aumentar</button> 
+			</div>)
+
+		
 	}
 }
---
+++ lab09/src/components/calculadora/calculadora.js
@@ -0,0 +1,34 @@
+import React, {Component} from 'react';
+import classes from './calculadora .css';
+
+export default class calculadora extends Component {
+
+    bootones = [
+                   {"1","2","3"},
+                   {"4","5","6"},
+                   {"7","8","9"} ]
+
+            constructor (props){
+                super(props)
+                this.state={
+                    txtsalida: '',
+                    num1:'',
+                    operador:''
+                }
+            }
+
+
+class calculadora extends Component {
+  render() {
+    return ( 
+
+        <div>
+      <calculadora valor={6} />
+      
+      </div>
+      
+    );
+  }
+}
+
+export default App;
